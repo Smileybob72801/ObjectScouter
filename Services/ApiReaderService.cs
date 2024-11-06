d10e37e3a83839ac35495b1a5b99ae9a0ac78781
Daniel <Smileybob72801@gmail.com>
2024-11-06 04:08:03 -0600

    Add data model for objects

--- Services/ApiReaderService.cs
+++ Services/ApiReaderService.cs
@@ -1,4 +1,6 @@
-﻿namespace NumberVerifier.Services
+﻿using System.Text.Json.Serialization;
+
+namespace NumberVerifier.Services
 {
 	internal interface IApiReaderService
 	{
@@ -22,4 +24,29 @@
 			return result ?? throw new InvalidOperationException("Result is null.");
 		}
 	}
+
+	public record Data(
+		[property: JsonPropertyName("color")] string color,
+		[property: JsonPropertyName("capacity")] string capacity,
+		[property: JsonPropertyName("capacity GB")] int? capacityGB,
+		[property: JsonPropertyName("price")] double? price,
+		[property: JsonPropertyName("generation")] string generation,
+		[property: JsonPropertyName("year")] int? year,
+		[property: JsonPropertyName("CPU model")] string CPUmodel,
+		[property: JsonPropertyName("Hard disk size")] string Harddisksize,
+		[property: JsonPropertyName("Strap Colour")] string StrapColour,
+		[property: JsonPropertyName("Case Size")] string CaseSize,
+		[property: JsonPropertyName("Color")] string Color,
+		[property: JsonPropertyName("Description")] string Description,
+		[property: JsonPropertyName("Capacity")] string Capacity,
+		[property: JsonPropertyName("Screen size")] double? Screensize,
+		[property: JsonPropertyName("Generation")] string Generation,
+		[property: JsonPropertyName("Price")] string Price
+	);
+
+	public record Root(
+		[property: JsonPropertyName("id")] string id,
+		[property: JsonPropertyName("name")] string name,
+		[property: JsonPropertyName("data")] Data data
+	);
 }
