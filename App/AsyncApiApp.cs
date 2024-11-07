626e34240ab9f58a0171c96567dc062bdc2d2ee1
Daniel <Smileybob72801@gmail.com>
2024-11-07 06:01:00 -0600

    Move AsyncApiApp.cs to App folder

---
+++ App/AsyncApiApp.cs
@@ -0,0 +1,35 @@
+﻿using AsyncRestApi.Model;
+using AsyncRestApi.Services;
+
+namespace AsyncRestApi.App
+{
+    internal class AsyncApiApp(IApiReaderService apiReaderService)
+    {
+        const string ApiBaseAddress = "https://api.restful-api.dev/";
+        const string RequestUri = "/objects";
+
+        private readonly IApiReaderService _apiReaderService = apiReaderService;
+
+        public void Run()
+        {
+            IEnumerable<Item> items = GetAllObjects(_apiReaderService).Result;
+
+            PrintObjects(items);
+        }
+
+        private void PrintObjects(IEnumerable<Item> objects)
+        {
+            foreach (Item item in objects)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private async Task<IEnumerable<Item>> GetAllObjects(IApiReaderService apiReaderService)
+        {
+            var result = await apiReaderService.ReadAsync(ApiBaseAddress, RequestUri);
+
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using AsyncRestApi.Model;
+﻿using AsyncRestApi.App;
 using AsyncRestApi.Services;
 
 namespace AsyncRestApi
@@ -21,35 +21,5 @@ namespace AsyncRestApi
 
 			Console.ReadKey();
         }
-	}
-
-	internal class AsyncApiApp(IApiReaderService apiReaderService)
-	{
-		const string ApiBaseAddress = "https://api.restful-api.dev/";
-		const string RequestUri = "/objects";
-
-		private readonly IApiReaderService _apiReaderService = apiReaderService;
-
-		public void Run()
-		{
-			IEnumerable<Item> items = GetAllObjects(_apiReaderService).Result;
-
-			PrintObjects(items);
-		}
-
-		private void PrintObjects(IEnumerable<Item> objects)
-		{
-			foreach (Item item in objects)
-			{
-				Console.WriteLine(item);
-			}
-		}
-
-		private async Task<IEnumerable<Item>> GetAllObjects(IApiReaderService apiReaderService)
-		{
-			var result = await apiReaderService.ReadAsync(ApiBaseAddress, RequestUri);
-
-			return result;
-		}
 	}
 }
