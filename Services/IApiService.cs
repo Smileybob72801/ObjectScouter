a81ee3f158379b0427e6b9bc48aa09329317e21f
Daniel <Smileybob72801@gmail.com>
2025-01-29 22:12:15 -0600

    Rename fix typo

--- Services/IApiService.cs
+++ Services/IApiService.cs
@@ -5,7 +5,7 @@ namespace ObjectScouter.Services
 	internal interface IApiService
 	{
 		Task PostAsync(string requestUri, Item item);
-		Task PutAync(string requestUri, Item item);
+		Task PutAsync(string requestUri, Item item);
 		Task<T> ReadAsync<T>(string requestUri);
 	}
 }
